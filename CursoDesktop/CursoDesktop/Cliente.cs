4f0965d5bd7b5ca2ed0e371f2aaafb1a175617c1
edvanmatta <edvan.zaza10@gmail.com>
2016-07-19 22:41:57 -0300

    Commit 1

---
+++ CursoDesktop/CursoDesktop/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesktop
+{
+    public class Cliente
+    {
+        public int codigo { get; set; }
+        public String nome { get; set; }
+        public int idade { get; set; }
+        public long telefone { get; set; }
+        public char sexo { get; set; }
+        public String endereco { get; set; }
+    }
+}
